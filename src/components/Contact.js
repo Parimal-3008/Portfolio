3d61ab7780e360e27d7d27b8f8ecd8a7fce25740
Parimal-3008 <parimalyeole30@gmail.com>
2023-02-22 15:27:41 +0530

    added loading component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,37 @@
-import React,{useRef} from 'react'
+import React,{useRef, useState} from 'react'
 import "../components/css/Contact.css";
+import ClipLoader from "react-spinners/ClipLoader";
+
 const  Contact=  React.forwardRef((props, ref) => {
   const formRef = useRef(null);
+  const [loading,setloading] = useState(false);
 
   const scriptUrl = "https://script.google.com/macros/s/AKfycbyRc12rwnXH04D5I50xwH9eDJqwwVLk83sq9DqlPYv10vbnDdC9iO4-2yt13qkS8N-v/exec"
 
   const handleclick = (e) =>{
       e.preventDefault()
+      setloading(true);
 
       fetch(scriptUrl, {method: 'POST', body: new FormData(formRef.current)})
       .then(res => {
-        alert("Message sent!!!");
+        setloading(false);
+        // alert("Message sent!!!");
         document.getElementById('sendername').value="";
         document.getElementById('sendermail').value="";
-        document.getElementById('sendermessage').value="";
-        
-          console.log("SUCCESSFULLY SUBMITTED")
+        document.getElementById('sendermessage').value="";        
+        console.log("SUCCESSFULLY SUBMITTED")
       })
       .catch(err => console.log(err))
   }
   
-
-
-
+  if(loading===true)
   return (
-    <div className='contactparent1' ref={ref}>
+    <div className='loaderparent1' ref={ref}>
+  <ClipLoader color="#FFD073" loading size="175px" aria-label="Loading Spinner" data-testid="loader" speedMultiplier="0.5"  />
+    </div>
+  );
+  else
+  return (<div className='contactparent1' ref={ref}>
     <div className='contactname2'>
     <hr className='projecthr' />
     Contact me
@@ -37,7 +44,7 @@ const  Contact=  React.forwardRef((props, ref) => {
     <div id='contactbuttondiv'><button className='contactsendmsg' onClick={ handleclick}>Send </button></div>
     </form>
     </div>
-  )
+  );
 });
 
 export default Contact
