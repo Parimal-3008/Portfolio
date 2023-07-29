0d60b75d94c21da78ec61be5b5384888da444872
Parimal-3008 <parimalyeole30@gmail.com>
2023-07-29 11:21:56 +0530

    About me updated

--- src/components/About.js
+++ src/components/About.js
@@ -10,15 +10,15 @@ const  About=  React.forwardRef((props, ref) => {
     <hr className='projecthr' />
     </div>
     <div className='MyDesc'>
-      <p>Hello! I am Parimal in my final year of engineering.</p>
-      <p>I am an enthusiastic web developer with a passion for creating dynamic and innovative web applications. I am graduating from NIT Surat with B.Tech degree.</p>
+      <p>Hello! I am Parimal.</p>
+      <p>I am an enthusiastic web developer with a passion for creating dynamic and innovative web applications. I graduated from NIT Surat.</p>
       <p>As a fresh graduate, I have strong understanding of web development principles and technologies. My passion for technology and creativity is reflected in my attention to detail and commitment.
       I am excited to further develop my skills and bring innovative solutions to future projects.  </p>
       <p>
         I have developed some web applications with the help of Reacts.js, Node.js, Express.js, MongoDB, and PostgreSQL.
         </p>
         <p>
-         Solving more than 1200 Data Structures and Algorithms problems on 
+         Solving more than 1500 Data Structures and Algorithms problems on 
         platforms like leetcode and codeforces helped me in improving my problem-solving skills.
       </p>
     </div>
